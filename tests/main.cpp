9b3f645c37276db2292b102fb6be286f31b46b77
SAMANYU07 <samanyu1003@gmail.com>
2025-10-22 23:33:51 +0530

    Implemented Json modification feature

--- tests/main.cpp
+++ tests/main.cpp
@@ -5,7 +5,13 @@ int main()
 {
     JsonParser parser("sample.json");
     // parser.printAllTokens();
-    std::cout << parser["iertg"];
+    // std::cout << parser["iertg"];
     // parser.printParsedJson();
+    Token token(TokenType::STRING, "INR");
+    JsonObject jsonData(token);
+    if (bool res = parser.modifyValue("preferred_currency", jsonData); !res)
+        std::cout << "modifyValue(preferred_currency) failed" << std::endl;
+    else
+        std::cout << "modifyValue(preferred_currency) success" << std::endl;
     return 0;
 }
